174b9750b209723c72782770256c9a8b60aa0368
Mocan Cosmin <cosmin_mocan@hotmail.com>
2021-01-27 21:52:45 +0200

    Updated start.sh
    A bit of cleanup and optimization.
    After some testing and some replies back and forth with a user on reddit, it seems that just unloading amdgpu is enough, this only works if you do stop sddm.service(or whatever display manager you use) and if you kill you audio service(in my case, pulse and pipewire)

--- start.sh
+++ start.sh
@@ -14,30 +14,25 @@ kill $pipewire_pid
 
 # Unbind VTconsoles
 echo 0 > /sys/class/vtconsole/vtcon0/bind
-# Some machines might have more than 1 virtual console. Add a line for each corresponding VTConsole
 echo 0 > /sys/class/vtconsole/vtcon1/bind
 
-# Unbind EFI-Framebuffer
-#echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
 
 # Avoid a race condition by waiting a couple of seconds. This can be calibrated to be shorter or longer if required for your system
-sleep 8
+sleep 4
 
 # Unload all Radeon drivers
 
 modprobe -r amdgpu
-modprobe -r gpu_sched
-modprobe -r ttm
-modprobe -r drm_kms_helper
-modprobe -r i2c_algo_bit
-modprobe -r drm
-modprobe -r snd_hda_intel
+#modprobe -r gpu_sched
+#modprobe -r ttm
+#modprobe -r drm_kms_helper
+#modprobe -r i2c_algo_bit
+#modprobe -r drm
+#modprobe -r snd_hda_intel
 
 # Unbind the GPU from display driver
 virsh nodedev-detach $VIRSH_GPU_VIDEO
 virsh nodedev-detach $VIRSH_GPU_AUDIO
-#virsh nodedev-detach $VIRSH_GPU_USB
-#virsh nodedev-detach $VIRSH_GPU_SERIAL
 
 # Load VFIO kernel module
 modprobe vfio
